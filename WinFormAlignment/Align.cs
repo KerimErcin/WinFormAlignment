fdc6f9cec770b19a1803f45a54647bdbeeac549f
Kerim Erçin Deveci <kerim3499@gmail.com>
2019-09-29 23:11:43 +0300

    Proje dosyası ekle.

---
+++ WinFormAlignment/Align.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormAlingment
+{
+    public static class Align
+    {
+        public static void TopRight(Control form, int left = 30, int top = 30)
+        {
+            form.Left = (Screen.PrimaryScreen.Bounds.Width - form.Width - left);
+            form.Top = top;
+        }
+
+        public static void TopLeft(Control form, int left = 30, int top = 30)
+        {
+            form.Location = new Point(left, top);
+        }
+
+        public static void BottomRight(Control form, int left = 30, int top = 30)
+        {
+            form.Left = (Screen.PrimaryScreen.WorkingArea.Size.Width - form.Width) - left;
+            form.Top = (Screen.PrimaryScreen.WorkingArea.Size.Height - form.Height) - top;
+        }
+
+        public static void BottomLeft(Control form, int left = 30, int top = 30)
+        {
+            Rectangle workingArea = Screen.GetWorkingArea(form);
+            form.Location = new Point(left, Screen.PrimaryScreen.WorkingArea.Bottom - (form.Height + top));
+        }
+    }
+}
+
